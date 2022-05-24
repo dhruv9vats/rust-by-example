1ad27fcae03a93ac269e795d0e9e27ea8d1009f2
Dhruv Vats <dhruv25vats@gmail.com>
2022-05-24 12:47:02 +0530

    Use ? operator

--- src/piglatin.rs
+++ src/piglatin.rs
@@ -1,16 +1,13 @@
 pub fn to_pig_latin(word: &str) -> Option<String> {
     let mut pig_latined_word = String::from(word);
     let vowels = vec!['a', 'e', 'i', 'o', 'u'];
-    match word.chars().next() {
-        Some(c) => {
-            if vowels.contains(&c) {
-                pig_latined_word.push_str("-hay");
-                Some(pig_latined_word)
-            } else {
-                pig_latined_word.push_str("-fay");
-                Some(pig_latined_word[1..].to_owned())
-            }
-        }
-        None => None,
+    let first_char = word.chars().next()?;
+
+    if vowels.contains(&first_char) {
+        pig_latined_word.push_str("-hay");
+        Some(pig_latined_word)
+    } else {
+        pig_latined_word.push_str("-fay");
+        Some(pig_latined_word[1..].to_owned())
     }
 }
